2a0923dd9e198a914ea84959cb53fd9e9ff1b7dd
mani-1509 <gajulamani10@gmail.com>
2026-09-01 02:00:04 +0530

    feat: add base HTML template and disable debug mode in application server

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=8000, debug=True) 
+    app.run(host='0.0.0.0', port=8000) 
 
